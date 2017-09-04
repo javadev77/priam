a2b21a39ac9502faa12e1d111db85610b0c3654f
jaafar <jaafar.belwidane@external.sacem.fr>
2017-09-04 11:30:56 +0200

    FIX PRIAM-76 : [US] Gestion de logs - chargement +Ajout de PriamValidationException

---
+++ priam-batch/src/main/java/fr/sacem/util/exception/PriamValidationException.java
@@ -0,0 +1,30 @@
+package fr.sacem.util.exception;
+
+/**
+ * Created by belwidanej on 30/08/2017.
+ */
+public class PriamValidationException extends Exception{
+
+    private final int lineNumber;
+    private final ErrorType errorType;
+
+    public static enum ErrorType {
+        FORMAT_FICHIER, FORMAT_ATTRIBUT
+    }
+
+
+
+    public PriamValidationException(int lineNumber, Exception ex, ErrorType errorType) {
+        super(ex);
+        this.lineNumber = lineNumber;
+        this.errorType = errorType;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
+    }
+}
