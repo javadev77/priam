5fd66ab7629316e8f121309febf78000b266c19e
jaafar <jaafar.belwidane@external.sacem.fr>
2017-09-08 12:06:27 +0200

    FIX PRIAM-76 : [US] Gestion de logs - chargement FIX PRIAM-127 : [US] Validation données obligatoires chargement - CP
    (correction recette interne )

--- priam-common/src/main/java/fr/sacem/util/FileUtils.java
+++ priam-common/src/main/java/fr/sacem/util/FileUtils.java
@@ -2,5 +2,5 @@ package fr.sacem.util;
 
 public class FileUtils {
     public static final String PREFIX_PRIV_SON_PH = "FF_PENEF_EXTRANA_EXTCPRIVSONPH";
-    public static final String PREFIX_PRIV_SON_RD = "FF_PENEF_EXTRANA_EXTCPRIVSONORD";
+    public static final String PREFIX_PRIV_SON_RD = "FF_PENEF_EXTRANA_EXTCPRIVSONRD";
 }
