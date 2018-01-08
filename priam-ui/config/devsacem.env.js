12587c14a5031055af03d743a92686af0854b522
BENMERZOUKA Habib <habib.benmerzouka@externe.sacem.fr>
2018-01-08 18:53:31 +0100

    Correction Bug : Batch Affectation CMS

--- priam-ui/config/devsacem.env.js
+++ priam-ui/config/devsacem.env.js
@@ -1,17 +1,17 @@
-/*module.exports = {
+module.exports = {
   NODE_ENV: '"production"',
   DEBUG_MODE: false,
   CONTEXT_ROOT : '"/priam"',
   CONTEXT_ROOT_PRIAM_COMMON : '"http://dev.sacem.fr/priam-common/"',
   CONTEXT_ROOT_PRIAM_CP : '"http://dev.sacem.fr/priam-cp/"',
   CONTEXT_ROOT_PRIAM_CMS : '"http://dev.sacem.fr/priam-cms/"'
-}*/
+}
 
-module.exports = {
+/*module.exports = {
   NODE_ENV: '"production"',
   DEBUG_MODE: false,
   CONTEXT_ROOT : '"/priam"',
   CONTEXT_ROOT_PRIAM_COMMON : '"http://dev.sacem.fr/priam-common-benmerzoukah/"',
   CONTEXT_ROOT_PRIAM_CP : '"http://dev.sacem.fr/priam-cp-benmerzoukah/"',
   CONTEXT_ROOT_PRIAM_CMS : '"http://dev.sacem.fr/priam-cms-benmerzoukah/"'
-}
+}*/
