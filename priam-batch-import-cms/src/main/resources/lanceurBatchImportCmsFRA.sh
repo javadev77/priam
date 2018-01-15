bb9e7cb0bc78ea2095633744521bb6492c555cbd
BENMERZOUKA Habib <habib.benmerzouka@externe.sacem.fr>
2018-01-15 14:29:32 +0100

    Preparation de la version 2.0.0 pour une Mise en intégration

--- priam-batch-import-cms/src/main/resources/lanceurBatchImportCmsFRA.sh
+++ priam-batch-import-cms/src/main/resources/lanceurBatchImportCmsFRA.sh
@@ -1,2 +1,1 @@
-
-/usr/java/JDK8/bin/java -jar -Dspring.profiles.active=production /usr1/priam/BatchImportCmsFRA/lib/@project.build.finalName@.jar
+/usr/java/JDK8/bin/java -jar -Dspring.profiles.active=production /usr1/priam/BatchImportCmsFRA/bin/@project.build.finalName@.jar
--- priam-batch-repartition/src/main/resources/lanceurBatchRepartition.sh
+++ priam-batch-repartition/src/main/resources/lanceurBatchRepartition.sh
@@ -1,1 +1,1 @@
-/usr/java/JDK8/bin/java -jar -Dspring.profiles.active=production /usr1/priam/BatchRepartition/lib/@project.build.finalName@.jar
+/usr/java/JDK8/bin/java -jar -Dspring.profiles.active=production /usr1/priam/BatchRepartition/bin/@project.build.finalName@.jar
