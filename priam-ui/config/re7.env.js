ae490d9cc44eee2c407195f30757300027ddcec5
BENMERZOUKA Habib <habib.benmerzouka@externe.sacem.fr>
2018-01-23 11:11:17 +0100

    MAJ de la conf des URLs des services REST metier sur INTEGRATION - context root : http://hochon.sacem.fr:10700

--- priam-ui/config/re7.env.js
+++ priam-ui/config/re7.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   DEBUG_MODE: false,
   CONTEXT_ROOT : '"/priam"',
-  CONTEXT_ROOT_PRIAM_COMMON : '"http://hochon.sacem.fr:10000/priam-common/"',
-  CONTEXT_ROOT_PRIAM_CP : '"http://hochon.sacem.fr:10000/priam-cp/"',
-  CONTEXT_ROOT_PRIAM_CMS : '"http://hochon.sacem.fr:10000/priam-cms/"'
+  CONTEXT_ROOT_PRIAM_COMMON : '"http://hochon.sacem.fr:10700/priam-common/"',
+  CONTEXT_ROOT_PRIAM_CP     : '"http://hochon.sacem.fr:10700/priam-cp/"',
+  CONTEXT_ROOT_PRIAM_CMS    : '"http://hochon.sacem.fr:10700/priam-cms/"'
 }
