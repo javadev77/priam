93bfa18764398801ecd20747802eb409e221cb3f
BENMERZOUKA Habib <habib.benmerzouka@externe.sacem.fr>
2018-01-23 10:47:22 +0100

    Changement de la config http des services metiers

--- priam-ui/config/dev.env.js
+++ priam-ui/config/dev.env.js
@@ -4,8 +4,8 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   DEBUG_MODE: true,
-  CONTEXT_ROOT_PRIAM_COMMON : '"http://localhost:8080/priam-common-embouazzar/"',
-  CONTEXT_ROOT_PRIAM_CP : '"http://localhost:8080/priam-cp-embouazzar//"',
-  CONTEXT_ROOT_PRIAM_CMS : '"http://localhost:8080/priam-cms-embouazzar/"'
+  CONTEXT_ROOT_PRIAM_COMMON : '"http://P93141.sacem.fr:8080/priam-common/"',
+  CONTEXT_ROOT_PRIAM_CP : '"http://P93141.sacem.fr:8080/priam-cp/"',
+  CONTEXT_ROOT_PRIAM_CMS : '"http://P93141.sacem.fr:8080/priam-cms/"'
 })
 
