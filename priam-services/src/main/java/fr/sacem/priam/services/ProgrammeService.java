49997918a40cb9db87e836ae09087bfffb8e343d
BENMERZOUKA Habib <habib.benmerzouka@externe.sacem.fr>
2018-03-23 18:05:40 +0100

    Livraison en integration PRIAM MEI v2.0.5 : ============================================ Changelog :
    PRIAM-165 : Selection - Tableau saisissable
    PRIAM-164 : Creation Journal d'evenements

--- priam-services/src/main/java/fr/sacem/priam/services/ProgrammeService.java
+++ priam-services/src/main/java/fr/sacem/priam/services/ProgrammeService.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
