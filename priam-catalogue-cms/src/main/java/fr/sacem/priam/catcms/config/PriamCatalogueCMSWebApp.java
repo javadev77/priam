1ef05e63ce81d888e4839a4462af5ecd40377d2a
BENMERZOUKA Habib <habib.benmerzouka@externe.sacem.fr>
2018-09-25 17:05:57 +0200

    Ajouter la page de sante sur priam-catalogue-cms

--- priam-catalogue-cms/src/main/java/fr/sacem/priam/catcms/config/PriamCatalogueCMSWebApp.java
+++ priam-catalogue-cms/src/main/java/fr/sacem/priam/catcms/config/PriamCatalogueCMSWebApp.java
@@ -1,5 +1,6 @@
 package fr.sacem.priam.catcms.config;
 
+import fr.sacem.priam.common.config.HealthConfig;
 import fr.sacem.priam.model.config.JpaConfiguration;
 import fr.sacem.priam.security.config.RestMvcConfig;
 import fr.sacem.priam.security.config.SecurityRestConfiguration;
@@ -8,7 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
-@Import(value = {RestMvcConfig.class, SecurityRestConfiguration.class, JpaConfiguration.class})
+@Import(value = {RestMvcConfig.class, SecurityRestConfiguration.class, JpaConfiguration.class, HealthConfig.class})
 @SpringBootApplication(scanBasePackages = {"fr.sacem.priam.common", "fr.sacem.priam.catcms"})
 public class PriamCatalogueCMSWebApp extends SpringBootServletInitializer {
 
