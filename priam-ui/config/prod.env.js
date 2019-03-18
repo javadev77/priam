284380c0e4881c74d8aa6995f485123c942b3380
BENMERZOUKA Habib <habib.benmerzouka@externe.sacem.fr>
2019-03-18 15:58:45 +0100

    v2.1.14 : BugFix Changement des URL https en prod

--- priam-ui/config/prod.env.js
+++ priam-ui/config/prod.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   DEBUG_MODE: false,
   CONTEXT_ROOT : '"/priam"',
-  CONTEXT_ROOT_PRIAM_COMMON : '"http://production-intranet.sacem.fr/priam-common/"',
-  CONTEXT_ROOT_PRIAM_CP : '"http://production-intranet.sacem.fr/priam-cp/"',
-  CONTEXT_ROOT_PRIAM_CMS : '"http://production-intranet.sacem.fr/priam-cms/"'
+  CONTEXT_ROOT_PRIAM_COMMON : '"https://production-intranet.sacem.fr/priam-common/"',
+  CONTEXT_ROOT_PRIAM_CP : '"https://production-intranet.sacem.fr/priam-cp/"',
+  CONTEXT_ROOT_PRIAM_CMS : '"https://production-intranet.sacem.fr/priam-cms/"'
 }
