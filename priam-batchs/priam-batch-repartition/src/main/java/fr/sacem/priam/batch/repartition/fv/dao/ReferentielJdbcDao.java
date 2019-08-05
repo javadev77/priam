c9ee746671ef503370d462029875f027dfb061c6
embouazzar <rachid.embouazza@externe.sacem.fr>
2019-08-05 16:45:44 +0200

    PRIAM-498

--- priam-batchs/priam-batch-repartition/src/main/java/fr/sacem/priam/batch/repartition/fv/dao/ReferentielJdbcDao.java
+++ priam-batchs/priam-batch-repartition/src/main/java/fr/sacem/priam/batch/repartition/fv/dao/ReferentielJdbcDao.java
@@ -1,13 +1,17 @@
 package fr.sacem.priam.batch.repartition.fv.dao;
 
 import fr.sacem.priam.batch.repartition.fv.domain.ReferentielParticipation;
+import fr.sacem.priam.batch.repartition.fv.mapper.ReferentielParticipationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -23,6 +27,38 @@ public class ReferentielJdbcDao {
     public List<ReferentielParticipation> getReferentielByIde12AndCdeTypUtil(Long ide12, String cdeTypUtil){
         String sql = "SELECT r.ide12, r.cdeTypUtil, r.rionPaiementMax FROM PRIAM_REFERENTIEL_PARTICIPATION_FDS r WHERE r.ide12=? AND r.cdeTypUtil=?";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ReferentielParticipation.class), ide12, cdeTypUtil);
+    }
+
+    @Transactional(readOnly = true, value = "transactionManager")
+    public List<ReferentielParticipation> getIde12RepartisByNumProg(String numeroProgramme){
+        String sql = "SELECT LPFV.ide12, LPFV.cdeTypUtil, LPFV.rionEffet " +
+                "FROM PRIAM_LIGNE_PREPREP_FV LPFV " +
+                "WHERE LPFV.numProg=? " +
+                "GROUP BY LPFV.ide12 ";
+        return jdbcTemplate.query(sql, new ReferentielParticipationMapper(), numeroProgramme);
+    }
+
+    @Transactional(value = "transactionManager")
+    public void enregistrerReferentielParticipation(ReferentielParticipation referentielParticipation){
+        String sql = "INSERT INTO PRIAM_REFERENTIEL_PARTICIPATION_FDS (ide12, cdeTypUtil, rionPaiementMax) VALUES (?,?,?)";
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setLong(1, referentielParticipation.getIde12());
+            stmt.setString(2, referentielParticipation.getCdeTypUtil());
+            stmt.setInt(3, referentielParticipation.getRionPaiementMax());
+            return stmt;
+        });
+    }
+
+    public void updateReferentielParticipation(ReferentielParticipation referentielParticipation){
+        String sql = "UPDATE PRIAM_REFERENTIEL_PARTICIPATION_FDS " +
+                "set rionPaiementMax =? " +
+                "where ide12 =? and cdeTypUtil =?";
+        jdbcTemplate.update(sql, stmt -> {
+            stmt.setInt(1, referentielParticipation.getRionPaiementMax());
+            stmt.setLong(2, referentielParticipation.getIde12());
+            stmt.setString(3, referentielParticipation.getCdeTypUtil());
+        });
     }
 
 }
--- priam-batchs/priam-batch-repartition/src/main/java/fr/sacem/priam/batch/repartition/service/RepartitionItemProcessor.java
+++ priam-batchs/priam-batch-repartition/src/main/java/fr/sacem/priam/batch/repartition/service/RepartitionItemProcessor.java
@@ -1,7 +1,17 @@
 package fr.sacem.priam.batch.repartition.service;
 
+import fr.sacem.priam.batch.common.dao.ProgrammeBatchDao;
+import fr.sacem.priam.batch.common.domain.Programme;
 import fr.sacem.priam.batch.common.domain.Repartition;
 import fr.sacem.priam.batch.common.util.valdiator.repartition.RepartitionSpringValidator;
+import fr.sacem.priam.batch.repartition.fv.dao.ReferentielJdbcDao;
+import fr.sacem.priam.batch.repartition.fv.domain.ReferentielParticipation;
+import fr.sacem.priam.common.TypeUtilisationEnum;
+import fr.sacem.priam.model.dao.jpa.JournalBatchDao;
+import fr.sacem.priam.model.domain.Journal;
+import fr.sacem.priam.model.domain.SituationApres;
+import fr.sacem.priam.model.domain.SituationAvant;
+import fr.sacem.priam.model.journal.JournalBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepExecution;
@@ -13,6 +23,8 @@ import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -21,16 +33,34 @@ import java.util.Set;
 
 public class RepartitionItemProcessor implements ItemProcessor<Repartition, Repartition> {
 
-    private static final Logger log = LoggerFactory.getLogger(RepartitionItemProcessor.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RepartitionItemProcessor.class);
 
     public static final String MESSAGE_FORMAT = "Ligne '%s': Le champ \"%s\" avec la valeur \"%s\" n'a pas le bon format attendu";
     public static final String MESSAGE_CHAMPS_OBLIGATOIRE = "Ligne '%s': Le champ %s est obligatoire et non renseigné";
     public static final String REPARTITION_ERRORS = "repartition-errors";
+    public static final String FAMILLE_VALORISATION = "FDSVAL";
+    public static final String TYPE_REPART_AYANT_DROIT = "AYANT_DROIT";
+
+    public static final String VALIDATION ="Validation programme";
+    public static final String REPARTITION ="Répartition";
+    private static final String PRG_VALIDE = "Validé";
+    private static final String PRG_MIS_EN_REPART = "Mis en répartition";
+    private static final String PRG_REPARTI = "Réparti";
+
 
     private ExecutionContext executionContext;
 
     @Autowired
     private RepartitionSpringValidator validator;
+
+    @Autowired
+    private ReferentielJdbcDao referentielJdbcDao;
+
+    @Autowired
+    ProgrammeBatchDao programmeBatchDao;
+
+    @Autowired
+    private JournalBatchDao journalBatchDao;
 
     @BeforeStep
     public void beforeStep(StepExecution stepExecution) {
@@ -43,17 +73,27 @@ public class RepartitionItemProcessor implements ItemProcessor<Repartition, Repa
 
         Set<String> errorSet = (Set<String>) executionContext.get(REPARTITION_ERRORS);
 
-        executionContext.put("numeroProgramme", repartition.getNumeroProgramme());
+//        executionContext.put("numeroProgramme", repartition.getNumeroProgramme());
 
         BindingResult errors = new BeanPropertyBindingResult(repartition, "repartition-"+ repartition.getLineNumber());
         validator.validate(repartition, errors);
         //gestion des status de programme venant de FELIX
         if(repartition.getStatus()!=null && !repartition.getStatus().equals("")){
+            Optional<String> optionalNumProg = Optional.ofNullable(repartition.getNumeroProgramme());
             if(repartition.getStatus().equalsIgnoreCase("OK")) {
                 repartition.setStatus("REPARTI");
+                if(optionalNumProg.isPresent()){
+                    Programme programme = programmeBatchDao.findByNumProg(optionalNumProg.get());
+                    if(FAMILLE_VALORISATION.equals(TypeUtilisationEnum.getValue(programme.getTypeUtilisation()).getCodeFamille())
+                            && !TYPE_REPART_AYANT_DROIT.equals(programme.getTypeRepart())){
+                        updateReferentiel(repartition.getNumeroProgramme());
+                    }
+                    journaliserRetourFelix(optionalNumProg.get(), REPARTITION);
+                }
             }
             else if(repartition.getStatus().equalsIgnoreCase("KO")){
                 repartition.setStatus("VALIDE");
+                journaliserRetourFelix(optionalNumProg.get(), VALIDATION);
             }
         }
         if (!errors.hasErrors()) {
@@ -72,20 +112,45 @@ public class RepartitionItemProcessor implements ItemProcessor<Repartition, Repa
         return repartition;
     }
 
-    private String getValeurDuChamp(String message) {
-        String text = message.split("'")[4];
-        return text.substring(text.indexOf('[') + 1, text.indexOf(']') );
+    private void journaliserRetourFelix(String numProg, String evenement) {
+        JournalBuilder journalBuilder = new JournalBuilder(numProg,null, null);
+        Journal journal = journalBuilder.addEvenement(evenement).build();
+        SituationAvant situationAvant = new SituationAvant();
+        situationAvant.setSituation(PRG_MIS_EN_REPART);
+        journal.getListSituationAvant().add(situationAvant);
+        if(REPARTITION.equals(evenement)){
+            SituationApres situationApres = new SituationApres();
+            situationApres.setSituation(PRG_REPARTI);
+            journal.getListSituationApres().add(situationApres);
+        } else {
+            SituationApres situationApres = new SituationApres();
+            situationApres.setSituation(PRG_VALIDE);
+            journal.getListSituationApres().add(situationApres);
+        }
+        long idJournal = journalBatchDao.saveJournal(journal);
+        journalBatchDao.saveSituationAvantJournal(journal.getListSituationAvant(), idJournal);
+        journalBatchDao.saveSituationApresJournal(journal.getListSituationApres(), idJournal);
     }
 
-    private String getNomDuChamp(String message) {
-
-        return message.split("'")[3];
-
+    private void updateReferentiel(String numProg) {
+        try {
+            List<ReferentielParticipation> ide12RepartisByNumProg = referentielJdbcDao.getIde12RepartisByNumProg(numProg);
+            ide12RepartisByNumProg.stream().forEach(ref -> {
+                List<ReferentielParticipation> listReferentiel = referentielJdbcDao.getReferentielByIde12AndCdeTypUtil(ref.getIde12(),
+                        ref.getCdeTypUtil());
+                if(listReferentiel.isEmpty()){
+                    referentielJdbcDao.enregistrerReferentielParticipation(ref);
+                } else {
+                    if(ref.getRionPaiementMax() > listReferentiel.stream().findFirst().get().getRionPaiementMax()){
+                        referentielJdbcDao.updateReferentielParticipation(ref);
+                    }
+                }
+            });
+        } catch (Exception e) {
+            //todo : stopper le batch si la maj du référentiel se passe mal ?
+            LOGGER.error(String.format("Erreur lors de la mise à jour du référentiel participant pour les oeuvres du programme %s !!", numProg), e);
+        }
     }
-
-    /*@BeforeStep
-    public void beforeStep(StepExecution stepExecution) {
-        this.executionContext = stepExecution.getExecutionContext();
-    }*/
-
+    
+    
 }
